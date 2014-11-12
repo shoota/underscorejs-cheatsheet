50a68486ba149f50cd1948a7acc6a93f3fbe1f94
shoota <shoota.dev@gmail.com>
2014-11-12 12:00:10 +0900

    add index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+var _ = require('underscore');
+
+console.log('\n\nThis is underscore.js samples.\n');
+console.log('version of underscore.js --> ' + _.VERSION);
+// console.log('All of underscore function');
+// _.each(_.functions(_), function(s){console.log('\t'+s)});
+
+console.log('See detail `https://github.com/shoota/underscorejs-cheatsheet`');
