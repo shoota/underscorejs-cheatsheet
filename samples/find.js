1ffaeb03aa2977893d18457f1af982462c00f555
shoota <shoota.dev@gmail.com>
2014-11-12 12:55:33 +0900

    add find

---
+++ samples/find.js
@@ -0,0 +1,12 @@
+var _ = require('underscore');
+var result;
+
+result = _.find([3,4,5,6,7,8,9], function(num){return num%2 == 0;});
+console.log(result);
+
+var objAry = [{name: "Mario",  type:"human" },
+              {name: "Troopa", type:"turtle"},
+              {name: "Koopa",  type:"turtle"},
+              {name: "Luigi",  type:"human" }];
+result = _.find(objAry,function(obj){return obj.type == 'turtle';});
+console.log(result);
